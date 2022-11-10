e2fe04be2be47445fc0bbbe8761ec8d573e8648d
GiacomoDiNardo <vest_95@hotmail.it>
2022-11-10 14:46:39 +0100

    milestone 3

--- Program.cs
+++ Program.cs
@@ -1,64 +1,101 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 
-Console.WriteLine("Inserisci nome evento");
-string titolo = Console.ReadLine();
+Console.WriteLine("Qual è il titolo del Programma eventi?");
+string titoloProgramma = Console.ReadLine();
 
-Console.WriteLine("Inserisci data evento in formato (dd/mm/yyyy)");
-DateTime data = Convert.ToDateTime(Console.ReadLine());
+ProgrammaEventi programma = new ProgrammaEventi(titoloProgramma);
 
-Console.WriteLine("Inserisci capienza massima evento");
-int capienza = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
+Console.WriteLine("Quanti eventi vuoi aggiungere?");
+int numEventi = Convert.ToInt32(Console.ReadLine());
 
-try
+while ( programma.NumeroEventi() < numEventi)
 {
-    Evento evento1 = new Evento(titolo, data, capienza);
-    Console.WriteLine("Dati evento:");
-    Console.WriteLine("Titolo evento: {0}", evento1.Titolo);
-    Console.WriteLine("Numero di posti totali: {0}", evento1.Capienza);
-    Console.WriteLine("L'evento si svolgerà: {0}", evento1.ToString());
+    Console.WriteLine("Inserisci nome evento");
+    string titoloEvento = Console.ReadLine();
 
+    Console.WriteLine("Inserisci data evento in formato (dd/mm/yyyy)");
+    DateTime dataEvento = Convert.ToDateTime(Console.ReadLine());
+
+    Console.WriteLine("Inserisci capienza massima evento");
+    int capienzaEvento = Convert.ToInt32(Console.ReadLine());
     Console.WriteLine();
 
-    Console.WriteLine("Quanti posti desideri prenotare?");
-    int prenotati = Convert.ToInt32(Console.ReadLine());
-    evento1.PrenotaPosti(prenotati);
-    Console.WriteLine("numero di posti prenotati: {0}", evento1.PostiPrenotati);
-
-    bool continua = true;
-    while(continua)
+    
+    try
     {
-        Console.WriteLine("Numero posti ancora disponibili: {0}", evento1.PostiDisponibili());
-        Console.WriteLine();
+        Evento evento = new Evento(titoloEvento, dataEvento, capienzaEvento);
 
-
-        Console.WriteLine("Vuoi disdire dei posti? (si/no)");
-        string risposta = Console.ReadLine();
-        int disdetti;
-
-        switch (risposta)
-        {
-            case "si":
-                Console.WriteLine("Inserisci numero posti da disdire");
-                disdetti = Convert.ToInt32(Console.ReadLine());
-                evento1.DisdiciPosti(disdetti);
-                Console.WriteLine();
-                Console.WriteLine("numero di posti prenotati: {0}", evento1.PostiPrenotati);
-                break;
-
-            case "no":
-                Console.WriteLine("Numero posti prenotati: {0}", evento1.PostiPrenotati);
-                continua = false;
-                break;
-        }
+        programma.AggiungiEvento(evento);
     }
-
-} catch (GestoreEventiException e)
-{
-    Console.WriteLine(e.Message);
+    catch (GestoreEventiException e)
+    {
+        Console.WriteLine(e.Message);
+    }
+    
+    
 }
 
+Console.WriteLine("Il programma ha {0} eventi", programma.NumeroEventi());
+Console.WriteLine();
+programma.StampaProgramma();
 
 
 
+//Console.WriteLine("Inserisci nome evento");
+//string titolo = Console.ReadLine();
+
+//Console.WriteLine("Inserisci data evento in formato (dd/mm/yyyy)");
+//DateTime data = Convert.ToDateTime(Console.ReadLine());
+
+//Console.WriteLine("Inserisci capienza massima evento");
+//int capienza = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine();
+
+//try
+//{
+//    Evento evento1 = new Evento(titolo, data, capienza);
+//    Console.WriteLine("Dati evento:");
+//    Console.WriteLine("Titolo evento: {0}", evento1.Titolo);
+//    Console.WriteLine("Numero di posti totali: {0}", evento1.Capienza);
+//    Console.WriteLine("L'evento si svolgerà: {0}", evento1.ToString());
+
+//    Console.WriteLine();
+
+//    Console.WriteLine("Quanti posti desideri prenotare?");
+//    int prenotati = Convert.ToInt32(Console.ReadLine());
+//    evento1.PrenotaPosti(prenotati);
+//    Console.WriteLine("numero di posti prenotati: {0}", evento1.PostiPrenotati);
+
+//    bool continua = true;
+//    while(continua)
+//    {
+//        Console.WriteLine("Numero posti ancora disponibili: {0}", evento1.PostiDisponibili());
+//        Console.WriteLine();
+
+
+//        Console.WriteLine("Vuoi disdire dei posti? (si/no)");
+//        string risposta = Console.ReadLine();
+//        int disdetti;
+
+//        switch (risposta)
+//        {
+//            case "si":
+//                Console.WriteLine("Inserisci numero posti da disdire");
+//                disdetti = Convert.ToInt32(Console.ReadLine());
+//                evento1.DisdiciPosti(disdetti);
+//                Console.WriteLine();
+//                Console.WriteLine("numero di posti prenotati: {0}", evento1.PostiPrenotati);
+//                break;
+
+//            case "no":
+//                Console.WriteLine("Numero posti prenotati: {0}", evento1.PostiPrenotati);
+//                continua = false;
+//                break;
+//        }
+//    }
+
+//} catch (GestoreEventiException e)
+//{
+//    Console.WriteLine(e.Message);
+//}
---
+++ ProgrammaEventi.cs
@@ -0,0 +1,54 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class ProgrammaEventi
+{
+    public string Titolo { get; set; }
+    List<Evento> Eventi { get; set; }
+
+    public ProgrammaEventi(string titolo)
+    {
+        Titolo = titolo;
+        Eventi = new List<Evento>();
+    }
+
+    public void AggiungiEvento(Evento evento)
+    {
+        Eventi.Add(evento);
+    }
+
+    public Evento InDataEventi(DateTime data)
+    {
+        foreach (Evento evento in Eventi)
+        {
+            if (evento.Data == data)
+            {
+                return evento;
+            }
+        }
+        return null;
+    }
+
+    public static void StampaInConsole (List<Evento> eventi)
+    {
+        foreach (Evento evento in eventi)
+        {
+            Console.WriteLine("{1} - {0}", evento.Titolo, evento.Data.ToString("dd/MM/yyyy"));
+        }
+    }
+
+    public int NumeroEventi()
+    {
+        return Eventi.Count;
+    }
+
+    public void SvuotaLista()
+    {
+        Eventi.Clear();
+    }
+
+    public void StampaProgramma()
+    {
+        Console.WriteLine("{0} Programma evento:", Titolo);
+        StampaInConsole(Eventi);
+    }
+}
